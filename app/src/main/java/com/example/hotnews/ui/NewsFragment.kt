edf7dd9a4059b614300410c4651d921fda7c3435
NikiZ97 <sharonov.nik1997@gmail.com>
2019-04-21 10:21:44 +0300

    GenericAdapter for RecyclerView added

--- app/src/main/java/com/example/hotnews/ui/NewsFragment.kt
+++ app/src/main/java/com/example/hotnews/ui/NewsFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.hotnews.R
 import com.example.hotnews.api.response.ArticleResponse
 import com.example.hotnews.base.BaseFragment
-import com.example.hotnews.ui.adapters.NewsAdapter
+import com.example.hotnews.ui.adapters.GenericAdapter
 import kotlinx.android.synthetic.main.fragment_news.*
+import kotlinx.android.synthetic.main.layout_news_item.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NewsFragment : BaseFragment() {
@@ -38,7 +40,21 @@ class NewsFragment : BaseFragment() {
 
     private fun updateNewsList(articles: List<ArticleResponse>?) {
         newsRecyclerView.layoutManager = LinearLayoutManager(context)
-        newsRecyclerView.adapter = NewsAdapter(articles ?: listOf(), context!!) {}
+        newsRecyclerView.adapter = object : GenericAdapter<ArticleResponse>(articles ?: listOf()) {
+            override fun getViewHolder(view: View, viewType: Int): RecyclerView.ViewHolder {
+                return NewsViewHolder(view)
+            }
+
+            override fun getLayoutId(position: Int, obj: ArticleResponse) = R.layout.layout_news_item
+        }
+    }
+
+    private class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        GenericAdapter.Binder<ArticleResponse> {
+
+        override fun bind(data: ArticleResponse) {
+            itemView.articleName.text = data.title
+        }
     }
 
 }
